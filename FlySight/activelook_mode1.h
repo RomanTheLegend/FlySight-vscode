5eabfa2e30bb788dcdf700c74c74fd85719a16ff
Roman Arden <roman@upgration.com>
2026-05-16 16:36:23 +0200

    Basic competition mode

---
+++ FlySight/activelook_mode1.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+**                                                                        **
+**  FlySight 2 firmware                                                   **
+**  Copyright 2025 Bionic Avionics Inc.                                   **
+**                                                                        **
+**  This program is free software: you can redistribute it and/or modify  **
+**  it under the terms of the GNU General Public License as published by  **
+**  the Free Software Foundation, either version 3 of the License, or     **
+**  (at your option) any later version.                                   **
+**                                                                        **
+**  This program is distributed in the hope that it will be useful,       **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+**  GNU General Public License for more details.                          **
+**                                                                        **
+**  You should have received a copy of the GNU General Public License     **
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>. **
+**                                                                        **
+****************************************************************************
+**  Contact: Bionic Avionics Inc.                                         **
+**  Website: http://flysight.ca/                                          **
+****************************************************************************/
+
+#ifndef ACTIVELOOK_MODE1_H
+#define ACTIVELOOK_MODE1_H
+
+#include "activelook.h"
+
+void FS_ActiveLook_Mode1_Init(void);
+FS_ActiveLook_SetupStatus_t FS_ActiveLook_Mode1_Setup(void);
+void FS_ActiveLook_Mode1_Update(void);
+
+#endif /* ACTIVELOOK_MODE1_H */
